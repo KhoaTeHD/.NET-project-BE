b8fd8495373f73f799b54eae35ccab440f50e2a4
TeHD <dangkhoa014@gmail.com>
2024-10-28 00:12:05 +0700

    Add Service CategoryAPI.

---
+++ Services.CategoryAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.CategoryAPI.Models;
+
+namespace Services.CategoryAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Services.CategoryAPI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.CategoryAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public Boolean Status { get; set; }
+    }
+}
