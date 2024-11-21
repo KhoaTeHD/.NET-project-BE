25d248eeb142ff13364cc3ca86b78ca4e49ea977
Vo Van Hung <vovanhung2864@gmail.com>
2024-11-21 21:54:44 +0700

    Hung added cors

--- GatewaySolution/Program.cs
+++ GatewaySolution/Program.cs
@@ -1,9 +1,19 @@
-using GatewaySolution.Extensions;
+﻿using GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Thêm dịch vụ CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy => policy.WithOrigins("http://localhost:4200") // Cho phép Angular chạy trên http://localhost:4200
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()); // Cho phép sử dụng Cookie hoặc thông tin xác thực nếu cần
+});
 
 builder.AddAppAuthentication();
 builder.Services.AddOcelot();
@@ -12,6 +22,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// Sử dụng CORS
+app.UseCors("AllowAngularApp");
+
 app.UseOcelot();
+
 
 app.Run();
