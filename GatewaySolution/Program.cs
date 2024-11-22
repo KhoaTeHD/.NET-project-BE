432c912444c48f1054cb9991f9fea267cbfc53b3
TeHD <dangkhoa014@gmail.com>
2024-11-23 00:42:32 +0700

    Complete config Gateway

--- GatewaySolution/Program.cs
+++ GatewaySolution/Program.cs
@@ -6,12 +6,13 @@ using Ocelot.Values;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 
 app.Run();
